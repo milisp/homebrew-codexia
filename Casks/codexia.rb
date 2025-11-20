2e3492d907e35ac94865e7309dd52d88ea518418
milisp <milisp@proton.me>
2025-11-20 06:14:46 -0500

    Update Codexia Cask SHA256 checksums for Intel and ARM architectures

--- Casks/codexia.rb
+++ Casks/codexia.rb
@@ -1,14 +1,14 @@
 cask "codexia" do
-  version "0.12.0"
+  version "0.13.0"
 
   on_intel do
-    sha256 "2eefd66c3233ffc0735c0730dee690f2feb1df05aa9055d4b240e72cd89fb32a"
+    sha256 "41b23c1813af5c305586f13c2cbe93b06369731ba18c77537807d2155a385d69"
 
     url "https://github.com/codexia-team/codexia/releases/download/v#{version}/codexia_#{version}_x64.dmg"
   end
 
   on_arm do
-    sha256 "1dc4b0e02a1bd03f6d76346f6d903b3395df244dc33a8c46d237fd679fd2f1aa"
+    sha256 "30e4f8ccf1c8d07827126b3cba1d3e039045a2ae5fe0a28adf10c9bbdbd4ba25"
 
     url "https://github.com/codexia-team/codexia/releases/download/v#{version}/codexia_#{version}_aarch64.dmg"
   end
