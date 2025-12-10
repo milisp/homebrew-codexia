a1aa82bfed9e228bb6c09f6da6d67a74220db01c
milisp <milisp@proton.me>
2025-12-10 09:17:01 -0500

    Update Codexia Cask to version 0.17.0 and update SHA256 checksums

--- Casks/codexia.rb
+++ Casks/codexia.rb
@@ -1,14 +1,14 @@
 cask "codexia" do
-  version "0.16.0"
+  version "0.17.0"
 
   on_intel do
-    sha256 "822ca9967c327e0ecea4ced9de9e2e7fa9acf4f2f9fe36a92145a077ff53e83d"
+    sha256 "b2082233c59a86ffb953d1310740492f7e7dd10972ba923a6fd39fdc446efcef"
 
     url "https://github.com/milisp/codexia/releases/download/v#{version}/codexia_#{version}_x64.dmg"
   end
 
   on_arm do
-    sha256 "4f3061a5759b88af50d35e035621e0d0aa78c991ed6a94b360a505f86b8f7283"
+    sha256 "5900dbe04a8ea019f0c37761020573414f9b5cd348fecccd3c5747f8711e98c8"
 
     url "https://github.com/milisp/codexia/releases/download/v#{version}/codexia_#{version}_aarch64.dmg"
   end
