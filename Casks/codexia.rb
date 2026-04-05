53e88288a5c703659bb365c117f7e79b97eee80a
milisp <milisp@proton.me>
2026-04-05 12:32:28 -0400

    chore(cask): bump codexia to 0.28.2

--- Casks/codexia.rb
+++ Casks/codexia.rb
@@ -7,6 +7,12 @@ cask "codexia" do
     url "https://github.com/milisp/codexia/releases/download/v#{version}/codexia_#{version}_aarch64.dmg"
   end
 
+  on_intel do
+    sha256 "7b86d1189a6ad3c1fc749c7283d8bbfca4a3c94fe4d86da7c3522b0e600b00f6"
+
+    url "https://github.com/milisp/codexia/releases/download/v#{version}/codexia_#{version}_x64.dmg"
+  end
+
   name "Codexia"
   desc "A powerful GUI and Toolkit for Codex CLI + Claude Code. FileTree + prompt notepad + git worktree and more."
   homepage "https://github.com/milisp/codexia"
