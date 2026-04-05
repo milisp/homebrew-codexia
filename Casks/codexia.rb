45623bb96467271e69bfbf3299e3e45c6ebb2c9c
milisp <milisp@proton.me>
2026-04-04 20:43:42 -0400

    chore(cask): bump codexia to 0.28.1

--- Casks/codexia.rb
+++ Casks/codexia.rb
@@ -1,8 +1,8 @@
 cask "codexia" do
-  version "0.28.0"
+  version "0.28.1"
 
   on_arm do
-    sha256 "427c059e6ae34e1414bbe89b4a1ee9c99e1ec7b55b7932eb97f4d097ed44e41c"
+    sha256 "0f4d8a71bf0559b938303939e93eef041214aa36f5e2b9128978a423b89ca6a3"
 
     url "https://github.com/milisp/codexia/releases/download/v#{version}/codexia_#{version}_aarch64.dmg"
   end
